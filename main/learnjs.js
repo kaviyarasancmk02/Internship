299008ee2c6f6afc150bf234eb7ff42fd3251b33
kaviyarasancmk <cmkkaviyarasanmm@gmail.com>
2023-08-29 06:07:14 -0700

    add learnjs.html & learnjs.js files

---
+++ main/learnjs.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('button');
+const container = document.getElementById('container');
+const body = document.querySelector('body');
+// const h1 = document.querySelector('h1');
+body.className = "grid place-content-center w-full h-screen gap-10 bg-blue-200 gap-10";
+// h1.className = "bg-green-500 text-white text-5xl font-serif text-center p-4";
+button.className = "border-2 border-green-600 p-2 bg-gray-200 w-fit px-10";
+container.className = "bg-gray-300 p-10 border-2 border-gray-800 w-92 h-auto";
+
+const h1Elements = document.querySelectorAll('h1');
+
+// h1Elements.forEach((h1) => {
+//     h1.className = "bg-green-500 text-white text-5xl font-serif text-center p-4 m-2";
+// });
+
+
+// function changeText1(){
+//     document.getElementById('head1').innerHTML = "Wellcome";
+// }
+// function changeText2(){
+//     document.getElementById('head1').innerHTML = h1;
+// }
+
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(hhh => {
+    console.log(hhh);
+});
