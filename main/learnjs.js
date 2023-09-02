2358553cc18eed3a7182251a0c498b3d2fa96ddd
kaviyarasancmk <cmkkaviyarasanmm@gmail.com>
2023-09-02 11:02:33 -0700

    create one music web page

--- main/learnjs.js
+++ main/learnjs.js
@@ -1,31 +1,27 @@
-const button = document.getElementById('button');
-const container = document.getElementById('container');
-const body = document.querySelector('body');
-// const h1 = document.querySelector('h1');
-body.className = "grid place-content-center w-full h-screen gap-10 bg-blue-200 gap-10";
-// h1.className = "bg-green-500 text-white text-5xl font-serif text-center p-4";
-button.className = "border-2 border-green-600 p-2 bg-gray-200 w-fit px-10";
-container.className = "bg-gray-300 p-10 border-2 border-gray-800 w-92 h-auto";
+window.addEventListener('load', () => {
 
-const h1Elements = document.querySelectorAll('h1');
-
-// h1Elements.forEach((h1) => {
-//     h1.className = "bg-green-500 text-white text-5xl font-serif text-center p-4 m-2";
-// });
-
-
-// function changeText1(){
-//     document.getElementById('head1').innerHTML = "Wellcome";
-// }
-// function changeText2(){
-//     document.getElementById('head1').innerHTML = h1;
-// }
-
-
-
-
-const numbers = [1, 2, 3, 4, 5];
-
-numbers.forEach(hhh => {
-    console.log(hhh);
+    const sound = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const color = [
+        "green",
+        "red",
+        "orange",
+        "yellow",
+        "blue",
+        "cyan",
+    ]
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function(){
+            sound[index].currentTime = 0;
+            sound[index].play();
+            createBubble(index);
+        });
+    });
+    const createBubble = (index) => {
+        const bubble = document.createElement('div');
+        visual.appendChild(bubble);
+        bubble.style.background = color[index];
+        bubble.style.animation = "jump 1s ease";
+    }
 });
